ce96c5c3d414ed02d801b53bda18361eb30df35c
Joel Elrod <joelelrod246@gmail.com>
2023-06-21 15:56:17 -0400

    Added problems list for future reference
    Signed-off-by: Joel Elrod <joelelrod246@gmail.com>

--- lc_container_with_most_water/Program.cs
+++ lc_container_with_most_water/Program.cs
@@ -1,4 +1,14 @@
 ﻿
+
+// PROBLEMS
+
+// While it is often the case, the answer does not always depend on the highest line in the array.
+// This means that lines 16 - 32 are unnecessary.
+// An example of this is { 1, 2, 1 } where the expected output is 2 because of the first and third elements.
+
+// The double for-loop approach technically works, but is incredibly inefficient.
+// It also exceeds that alloted time-limit on Leetcode.
+
 int[] test = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
 Console.WriteLine($"Max area = {Solution.MaxArea(test)}");
 
