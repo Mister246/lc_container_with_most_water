b48ffd50e94e2bdf9575c3c5c4cc3422fed07b95
Joel Elrod <joelelrod246@gmail.com>
2023-06-21 14:28:12 -0400

    Initial Commit
    Signed-off-by: Joel Elrod <joelelrod246@gmail.com>

---
+++ lc_container_with_most_water/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+
+public class Solution
+{
+    static public int MaxArea(int[] height)
+    {
+        // get the highest line(s) in the array
+        // FOR (each highest line)
+            // 
+    }
+}
